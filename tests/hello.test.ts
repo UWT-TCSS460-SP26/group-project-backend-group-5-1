dbd381c45ce1fb72188611855b80fa5195c502c9
carsonrp <carsonrp@uw.edu>
2026-04-12 13:15:07 -0700

    added my  hello route after creating my branch from main

--- tests/hello.test.ts
+++ tests/hello.test.ts
@@ -7,4 +7,10 @@ describe('Hello Route', () => {
     expect(response.status).toBe(200);
     expect(response.body.message).toBe('Hello, TCSS 460!');
   });
+
+  it('GET /hello/carson-poirier — returns personalized greeting', async () => {
+    const response = await request(app).get('/hello/carson-poirier');
+    expect(response.status).toBe(200);
+    expect(response.body.greeting).toBe('Hello, Carson Poirier!');
+  });
 });
