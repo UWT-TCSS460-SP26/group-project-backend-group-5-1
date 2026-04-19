8e7fdac7cc208ba1e18281ed63b72a59cd0cca8f
Geovani Vasquez <geovav@uw.edu>
2026-04-18 17:08:01 -0700

    I changed the import so it imports from the right app

--- tests/__mocks__/movie/movie.popular.test.ts
+++ tests/__mocks__/movie/movie.popular.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import { app } from '../src/app';
+import { app } from '../../../src/app';
 const mockFetch = jest.fn();
 global.fetch = mockFetch;
 const mockPopularMoviesResponse = {
