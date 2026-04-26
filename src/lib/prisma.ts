fa567ba0e283d9211bc4e1b7dbc429768098c3d3
kylenngu135 <kylenthefrog@gmail.com>
2026-04-26 14:56:20 -0700

    prisma migration, prisma schema, and prisma seed created/updated

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,2 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-export const prisma = new PrismaClient();
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = process.env.DATABASE_URL!;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
